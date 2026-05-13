9dc6bad2877fc8cdc4e73a45119ba5aa7bf55631
ural <sevomax@gmail.com>
2026-05-13 18:12:13 -0400

    update to robots

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,11 +4,10 @@ import { SITE_URL } from '@/lib/seo';
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
-      {
-        userAgent: '*',
-        allow: '/',
-        disallow: ['/api/'],
-      },
+      { userAgent: '*', allow: '/', disallow: ['/api/'] },
+      { userAgent: 'AhrefsBot',  crawlDelay: 10 },
+      { userAgent: 'SemrushBot', crawlDelay: 10 },
+      { userAgent: 'MJ12bot',    disallow: '/' },
     ],
     sitemap: `${SITE_URL}/sitemap.xml`,
     host: SITE_URL,
