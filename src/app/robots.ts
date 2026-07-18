af5280cd1c004b33e5f52fa4b865eb68f5a084ff
ural <sevomax@gmail.com>
2026-07-18 16:04:47 -0400

    updated robots file

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,16 +4,20 @@ import { SITE_URL } from '@/lib/seo';
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
-      { userAgent: '*', allow: '/', disallow: ['/api/'] },
-      { userAgent: 'bingbot', allow: '/' },
-      { userAgent: 'AhrefsBot',  crawlDelay: 10 },
-      { userAgent: 'SemrushBot', crawlDelay: 10 },
-      { userAgent: 'MJ12bot',    disallow: '/' },
+      { 
+        userAgent: '*', 
+        allow: '/', 
+        disallow: ['/api/'] 
+      },
+      { 
+        userAgent: 'MJ12bot', 
+        disallow: '/' 
+      },
     ],
     sitemap: [
-      `${SITE_URL}/sitemap.xml`,
-      `${SITE_URL}/sitemap-dynamic.xml`,
-      `${SITE_URL}/sitemap-news.xml`,
+      'https://macrostance.com/sitemap.xml',
+      'https://macrostance.com/sitemap-dynamic.xml',
+      'https://macrostance.com/sitemap-news.xml',
     ],
     host: SITE_URL,
   };
